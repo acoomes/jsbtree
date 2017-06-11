2cf77c28711d4083d90f78869a2e7226417ddd02
acoomes <86drift@gmail.com>
2017-06-10 23:24:54 -0700

    Revise node structure, set up initial tree outline

--- btree.js
+++ btree.js
@@ -1,5 +1,24 @@
-var node = {
-  value: 100,
-  left: null,
-  right: null
+function Node() {
+  this.value: 100,
+  this.left: null,
+  this.right: null
 };
+
+function BinarySearchTree() {
+  this.root: null
+};
+
+BinarySearchTree.prototype = {
+  constructor: BinarySearchTree,
+  addNode: function(value) {
+    
+  },
+  removeNode: function(value) {
+    
+  },
+  locate: function(value) {
+    // TODO Set up found and currentNode variables
+    // TODO Loop through nodes starting with root, updating current as you go
+    // TODO Break loop and update found to true once requested value is found
+  },
+}
